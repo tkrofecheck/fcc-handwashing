3a08b4d17d0ed241f3a967eafc12ccdffead97c9
Tim Krofecheck <tkrofecheck@dotdashmdp.com>
2023-01-06 12:16:43 -0500

    got the timer code done

--- src/ProgressBar.svelte
+++ src/ProgressBar.svelte
@@ -1,3 +1,7 @@
+<script>
+    export let progress = 0; // prop with default value
+</script>
+
 <style>
     .progress-container {
         margin: 20px 0;
@@ -13,8 +17,8 @@
 
 <div bp="grid">
     <div bp="offset-5@md 4@md 12@sm" class="progress-container">
-        <div class="progress-bar" style="width: 20%;">
-            <span class="sr-only">%</span>
+        <div class="progress-bar" style="width: {progress}%;">
+            <span class="sr-only">{progress}%</span>
         </div>
     </div>
 </div>
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,5 +1,21 @@
 <script>
     import ProgressBar from "./ProgressBar.svelte";
+
+    $: progress = ((totalSeconds - secondsLeft) / totalSeconds) * 100;
+    const totalSeconds = 20;
+    let secondsLeft = totalSeconds;
+    let isRunning = false;
+    function startTimer() {
+        isRunning = true;
+        const timer = setInterval(() => {
+            secondsLeft -= 1;
+            if (secondsLeft == 0) {
+                clearInterval(timer);
+                isRunning = false;
+                secondsLeft = totalSeconds;
+            }
+        }, 1000);
+    }
 </script>
 
 <style>
@@ -12,12 +28,19 @@
         width: 100%;
         margin: 10px 0;
     }
+
+    .start:disabled {
+        background-color: rgb(194, 194, 194);
+        cursor: not-allowed;
+    }
 </style>
 
 <div bp="grid">
-    <h2 bp="offset-5@md 4@md 12@sm">Seconds Left:</h2>
+    <h2 bp="offset-5@md 4@md 12@sm">Seconds Left: {secondsLeft}</h2>
 </div>
 
-<ProgressBar />
+<ProgressBar {progress}/>
 
-<button class="start">Start</button>
+<div bp="grid">
+    <button disabled={isRunning} bp="offset-5@md 4@md 12@sm" class="start" on:click={startTimer}>Start</button>
+</div>
